4ab3cee65167c5d9d0b2841c618cc4189118a0b2
AlessandroRonconi <ronconi.alessandro46@gmail.com>
2026-08-19 17:13:39 +0200

    Template di funzioni pubbliche e service

---
+++ siw-festival/src/main/java/it/uniroma3/siw/siw_festival/service/SalaService.java
@@ -0,0 +1,10 @@
+package it.uniroma3.siw.siw_festival.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class SalaService {
+
+}
