8213aeb5f42dec6d1c55a0d984f5c6ebb8a1f55d
AlessandroRonconi <ronconi.alessandro46@gmail.com>
2026-08-24 13:46:18 +0200

    Fix piccolini e inizio funzioni utenti registrati

--- siw-festival/src/main/java/it/uniroma3/siw/siw_festival/service/CredentialsService.java
+++ siw-festival/src/main/java/it/uniroma3/siw/siw_festival/service/CredentialsService.java
@@ -1,10 +1,29 @@
 package it.uniroma3.siw.siw_festival.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import it.uniroma3.siw.siw_festival.model.Credentials;
+import it.uniroma3.siw.siw_festival.repository.CredentialsRepository;
 
 @Service
 @Transactional
 public class CredentialsService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final CredentialsRepository credentialsRepository;
+
+    public CredentialsService(CredentialsRepository credentialsRepository, PasswordEncoder passwordEncoder) {
+        this.credentialsRepository = credentialsRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public Credentials saveCredentials(Credentials credentials) {
+        credentials.setRole(Credentials.DEFAULT_ROLE);
+        credentials.setPassword(this.passwordEncoder.encode(credentials.getPassword()));
+        return this.credentialsRepository.save(credentials);
+    }
 
 }
